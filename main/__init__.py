de48ed94025716523d432d65923222a49ce0088e
Nemuroscythe <58225415+Nemuroscythe@users.noreply.github.com>
2022-12-01 15:51:39 +0100

    SB4 Add proper urls to CORS

--- main/__init__.py
+++ main/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('main.config')
 app.logger.debug(app.config.get('SQLALCHEMY_DATABASE_URI'))
 
-cors = CORS(app, origins=["http://localhost:3000"])
+cors = CORS(app, origins=["http://127.0.0.1:5173", "http://localhost:5173"])
 
 database = SQLAlchemy(app)
 database.init_app(app)
