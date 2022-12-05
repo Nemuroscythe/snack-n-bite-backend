e2a98e15b866d7129ac97ab581948a3feb2dfacd
JeremyA <58225415+Nemuroscythe@users.noreply.github.com>
2022-12-05 18:09:33 +0100

    SB9Extra Ajout de fonction pour create, delete et update des plats dans le repository

--- main/dishes/repository/dishesRepository.py
+++ main/dishes/repository/dishesRepository.py
@@ -5,3 +5,23 @@ from main.dishes.models.dish import Dish
 def get_dishes():
     row_dish_list = db.session.execute(db.select(Dish)).all()
     return row_dish_list
+
+
+def create_dish(dish):
+    db.session.add(dish)
+    db.session.commit()
+    return None
+
+
+def update_dish(dish):
+    dish_db = db.get_or_404(Dish, dish.id_dishes)
+    dish_db.content = dish.content
+    db.session.commit()
+    return None
+
+
+def delete_dish(dish_id):
+    dish_db = db.get_or_404(Dish, dish_id)
+    db.session.delete(dish_db)
+    db.session.commit()
+    return None
